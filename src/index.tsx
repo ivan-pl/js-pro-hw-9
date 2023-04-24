1f2ca8fd003871ae36aa47cdb39fff35045a3c52
ivan-pl <pleskaiv@gmail.com>
2023-04-24 15:25:28 +0300

    feat: add citySearch page

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
